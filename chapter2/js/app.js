6b566663dd6cf4437f105bee2ddc6396cbdd6025
WangDzh <wangdzh89@126.com>
2015-12-21 14:25:36 +0800

    ControllerTest

---
+++ chapter2/js/app.js
@@ -0,0 +1,14 @@
+var app = angular.module("myApp", []);
+app.controller("MyController", function($scope) {
+	$scope.clock = {
+		now: new Date()
+	};
+	var updateClock = function($scope) {
+		$scope.clock.now = new Date();
+	};
+	setInterval(function() {
+		// $scope.$apply(updateClock);
+		updateClock();
+	}, 1000);
+	updateClock();
+});
